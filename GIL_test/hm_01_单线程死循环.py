1ebf6aa8e0be10bd07442f162eab7bd5ea8e0c0f
xiaoxiao <xxhan2018@163.com>
2020-02-21 22:57:12 -0500

    add GIL_test

---
+++ GIL_test/hm_01_单线程死循环.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+
+# 主线程死循环
+
+while True:
+    pass
---
+++ GIL_test/hm_03_两个进程死循环.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+
+import multiprocessing
+
+
+def deadLoop():
+    while True:
+        pass
+
+#　子进程死循环
+p1 = multiprocessing.Process(target=deadLoop)
+p1.start()
+
+# 主进程死循环
+while True:
+    pass
